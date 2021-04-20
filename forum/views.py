162f6a0e68bc937c7d58205bff3fa9f5286be783
Grigoriy Hanin <43445998+haningrisha@users.noreply.github.com>
2021-04-20 10:34:13 +0300

    SubsectionList, ArticleList added

--- forum/views.py
+++ forum/views.py
@@ -1,8 +1,20 @@
 from django.views import generic
-from forum.models import Section
+from forum.models import Section, Subsection, Article
 
 
 class SectionList(generic.ListView):
     model = Section
     template_name = "forum/index.html"
     context_object_name = "sections"
+
+
+class SubsectionList(generic.DeleteView):
+    model = Subsection
+    template_name = "forum/subsection.html"
+    context_object_name = "subsection"
+
+
+class ArticleDetail(generic.DeleteView):
+    model = Article
+    template_name = "forum/article.html"
+    context_object_name = "article"
