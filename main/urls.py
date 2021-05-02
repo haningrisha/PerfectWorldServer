28796bbf3c4cc4f6ead2181c6eb80f8a798e979c
Grigoriy Hanin <43445998+haningrisha@users.noreply.github.com>
2021-05-02 18:34:50 +0300

    News added

--- main/urls.py
+++ main/urls.py
@@ -4,4 +4,5 @@ import main.views as views
 app_name = 'main'
 urlpatterns = [
    path('', views.index, name='index'),
+   path('start_game', views.start_game, name='start_game')
 ]
--- main/views.py
+++ main/views.py
@@ -1,10 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from forum.models import Article, Section, Subsection
 
 
 def index(request):
-    return render(request, "main/index.html")
+    news = get_news()
+    return render(request, "main/index.html", context={"news": news})
 
 
 def contacts(request):
     return HttpResponse("It's contacts page")
+
+
+def start_game(request):
+    return render(request, "main/start_game.html")
+
+
+def get_news():
+    section = Section.objects.filter(name="Основной раздел").first()
+    subsection = Subsection.objects.filter(name="Новости").filter(section_id=section.id).first()
+    news = Article.objects.filter(subsection_id=subsection.id).order_by('-date')
+    return news
